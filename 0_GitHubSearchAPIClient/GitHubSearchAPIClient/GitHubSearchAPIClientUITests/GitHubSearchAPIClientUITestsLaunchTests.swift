c1f67f48381a5aaedd0d109bca1d6c4c596899f0
IKEH1024 <pondau1024@gmail.com>
2022-01-21 01:10:18 +0900

    append project

---
+++ 0_GitHubSearchAPIClient/GitHubSearchAPIClient/GitHubSearchAPIClientUITests/GitHubSearchAPIClientUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  GitHubSearchAPIClientUITestsLaunchTests.swift
+//  GitHubSearchAPIClientUITests
+//
+//  Created by HIROKI IKEUCHI on 2022/01/21.
+//
+
+import XCTest
+
+class GitHubSearchAPIClientUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ios-test-complete-book
-iOSテスト全書の練習
+- iOSテスト全書の練習
+
+# HP
+
+- [iOSテストのバイブル「iOSテスト全書」！](https://peaks.cc/books/iOS_testing)
